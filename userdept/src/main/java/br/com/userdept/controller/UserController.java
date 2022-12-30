a29b58dfc23aba6351c45d5c731b2040b9476cff
WtHony <hony.cleyttonse@gmail.com>
2022-12-30 16:32:53 -0300

    ajuste controller

--- userdept/src/main/java/br/com/userdept/controller/UserController.java
+++ userdept/src/main/java/br/com/userdept/controller/UserController.java
@@ -1,8 +1,31 @@
 package br.com.userdept.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.userdept.model.User;
+import br.com.userdept.repositories.UserRepository;
+
 @RestController
+@RequestMapping(value = "/users")
 public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping
+    public List<User> findall() {
+        return repository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public User findByIdUser(@PathVariable Long id) {
+        return repository.findById(id).get();
+    }
 
 }
---
+++ userdept/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_departamento(name_dp) VALUES ('Gestao');
+INSERT INTO tb_departamento(name_dp) VALUES ('Informatica');
+
+INSERT INTO tb_user(departamento_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
+INSERT INTO tb_user(departamento_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
+INSERT INTO tb_user(departamento_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
+INSERT INTO tb_user(departamento_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
